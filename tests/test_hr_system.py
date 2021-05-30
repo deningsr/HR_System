4156e2ef977576a2d5db401711bba1b494c9445b
Denin Grcic <deningrcic2@gmail.com>
2021-05-29 20:30:10 -0700

    Updated employee report test

--- tests/test_hr_system.py
+++ tests/test_hr_system.py
@@ -1,4 +1,5 @@
 import hr_assign2.hr_system
+import datetime
 
 
 # def test_add_new_emp():
@@ -27,4 +28,29 @@ def test_sort_reports():
         {"ID": "7000", "Name": "Judy"},
         {"ID": "5000", "Name": "John"},
         {"ID": "4500", "Name": "Steve"},
+    ]
+
+
+def test_update_row():
+    readData = [
+        {"ID": "5000", "Name": "John"},
+        {"ID": "4500", "Name": "Steve"},
+        {"ID": "7000", "Name": "Judy"},
+    ]
+    assert hr_assign2.hr_system.update_row(readData, "5000", "Name", "Henry") == [
+        {"ID": "5000", "Name": "Henry"},
+        {"ID": "4500", "Name": "Steve"},
+        {"ID": "7000", "Name": "Judy"},
+    ]
+
+
+def test_list_current_employees():
+    readData = [
+        {"ID": "5000", "Name": "John", "End Date": "2025-01-01 00:00:00"},
+        {"ID": "4500", "Name": "Steve", "End Date": "2022-01-20 00:00:00"},
+        {"ID": "7000", "Name": "Judy", "End Date": "2015-02-11 00:00:00"},
+    ]
+    assert hr_assign2.hr_system.list_current_employees(readData) == [
+        {"ID": "5000", "Name": "John", "End Date": "2025-01-01 00:00:00"},
+        {"ID": "4500", "Name": "Steve", "End Date": "2022-01-20 00:00:00"},
     ]
